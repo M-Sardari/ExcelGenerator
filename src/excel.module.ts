7a08cf42314f4954733a6b9f59e509fae4922475
mohammad <m.sardari90@gmail.com>
2023-01-30 11:41:50 +0330

    fix bug in module

--- src/excel.module.ts
+++ src/excel.module.ts
@@ -1,16 +1,21 @@
+import { DynamicModule } from "@nestjs/common";
 import { EXCEL_SERVICE, ExcelModuleOptions } from "./config/excel-module-options.interface";
+import { ExcelService } from "./excel.service";
 
 export class ExcelModule {
-  static forRoot(options: ExcelModuleOptions) {
+  static forRoot(options: ExcelModuleOptions):DynamicModule {
     return {
       global: true,
       module: ExcelModule,
+      imports: [],
       providers: [
         {
           provide: EXCEL_SERVICE,
           useValue: options,
         },
-      ]
+        ExcelService
+      ],
+      exports:[ExcelService]
     };
   }
 }
